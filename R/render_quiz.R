fd7e79f249f114034637d6fdfd558aa8ef89781a
James Murray <james@murraylax.org>
2023-02-16 16:33:27 -0600

    working on canvasquizzeR

--- R/render_quiz.R
+++ R/render_quiz.R
@@ -26,14 +26,20 @@
 #' @param includeanswers Boolean, set equal to TRUE to include answers in the output, FALSE to not include answers in the output. Default value is TRUE.
 #'
 #' @param version Character, set to "a" or "b" for a version number. If there are more than two questions in any group of questions, for the name random number seed, the two versions will have two different questions. Default value is "a".
-
+#'
+#' @export
 render_pdf_quiz <- function(quiz.df, outfile, outfolder, quiz_title, quiz_subtitle="", instructor="", includeanswers=TRUE, version="a", seed=1) {
 
   outfile_pdf <- sprintf("%s.pdf", outfile)
 
+  template_file <- paste0(path.package("canvasquizzeR"), "/rmarkdown/templates/quiz-template.tex")
+
   markdown_file <- sprintf("%s/quiz-generate.Rmd", path.package("canvasquizzeR"))
-  rmarkdown::render(markdown_file, outputfile=outfile_pdf, output_dir=outfolder, intermediates_dir=outfolder,
-                    params=list(quiz.df=quiz.df, title=quiz_title, subtitle=quiz_subtitle, instructor=instructor, version=version, includeanswers=includeanswers, seed=seed))
+  rmarkdown::render(markdown_file,
+                    output_format = rmarkdown::pdf_document(template=template_file, keep_md=TRUE, keep_tex=TRUE),
+                    output_file=outfile_pdf, output_dir=outfolder, intermediates_dir=outfolder, clean=FALSE,
+                    params=list(quiz.df=quiz.df, title=quiz_title, subtitle=quiz_subtitle, instructor=instructor, version=version, includeanswers=includeanswers, seed=seed,
+                                template=template_file))
 
 }
 
